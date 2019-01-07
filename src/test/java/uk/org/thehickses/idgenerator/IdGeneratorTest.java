a7f4d2839cf98ddb47ee69eba4f5d54852d63efb
Jeremy Hicks <jerhicks@cisco.com>
2019-01-07 14:04:55 +0000

    Refactor

--- src/test/java/uk/org/thehickses/idgenerator/IdGeneratorTest.java
+++ src/test/java/uk/org/thehickses/idgenerator/IdGeneratorTest.java
@@ -13,13 +13,18 @@ public class IdGeneratorTest
     public void test()
     {
         IdGenerator generator = new IdGenerator(5, 1);
-        IntStream.rangeClosed(1, 5).forEach(i -> assertThat(generator.allocateId()).isEqualTo(i));
+        expectAllocations(generator, IntStream.rangeClosed(1, 5).toArray());
         expectExceptionOnAllocate(generator);
-        IntStream.of(3, 5, 4, 0).forEach(generator::freeId);
-        assertThat(generator.allocateId()).isEqualTo(3);
-        IntStream.of(4, 6, 1).forEach(generator::freeId);
-        IntStream.of(4, 5, 1).forEach(i -> assertThat(generator.allocateId()).isEqualTo(i));
+        free(generator, 3, 5, 4, 0);
+        expectAllocations(generator, 3);
+        free(generator, 4, 6, 1);
+        expectAllocations(generator, 4, 5, 1);
         expectExceptionOnAllocate(generator);
+    }
+
+    private void expectAllocations(IdGenerator generator, int... expectedResults)
+    {
+        IntStream.of(expectedResults).forEach(i -> assertThat(generator.allocateId()).isEqualTo(i));
     }
 
     private void expectExceptionOnAllocate(IdGenerator generator)
@@ -32,5 +37,10 @@ public class IdGeneratorTest
         catch (IllegalStateException ex)
         {
         }
+    }
+
+    private void free(IdGenerator generator, int... ids)
+    {
+        IntStream.of(ids).forEach(generator::freeId);
     }
 }
